5a436b723a6ed404099769e69a758794beb320b8
okor <jason.ormand1@gmail.com>
2015-01-25 21:01:13 -0500

    gruntifying this bad boy, adding scss build step

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    exec: {
+      build: {
+        command: 'npm run build',
+        stdout: true,
+        stderr: true
+      }
+    },
+
+    sass: {
+      dist: {
+        files: {
+          'lib/perfBar.css': 'lib/scss/perfBar.scss'
+        }
+      }
+    },
+
+    watch: {
+      files: ['lib/scss/perfBar.scss'],
+      tasks: ['exec:build']
+    }
+
+  });
+
+
+  grunt.loadNpmTasks('grunt-exec');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['sass', 'watch']);
+
+};
